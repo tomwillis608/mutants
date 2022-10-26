0e171f3e5167dd1b2f39348fde01c463a1d364aa
Tom Willis <twillis@flexion.us>
2022-10-26 13:04:30 -0500

    Add additional opportunities for mutants

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,22 +1,30 @@
 from mutants.calculations import calculate_metric
 from mutants.calculations import choose_metric
+from mutants.calculations import METRIC_LOC
+from mutants.calculations import METRIC_MTTR
 
 def test_calculate_metric_happy() -> None:
     """Test calculate_metric happy path."""
     result = calculate_metric()
-    assert result == 2
-
-def test_chose_metric_happy() -> None:
-    """Test choose_metric happy path."""
-    result = choose_metric()
-    assert result == 21
+    assert result is not None
 
 def test_calculate_metric_8_8() -> None:
     """Test calculate_metric path."""
-    result = calculate_metric(8, 8)
-    assert result == 2
+    result = calculate_metric(88, 88)
+    assert result > 0
 
-def test_chose_metric_10() -> None:
-    """Test choose_metric path."""
-    result = choose_metric(1, 2)
-    assert result == 10
+def test_chose_metric_mttr() -> None:
+    """Test choose_metric happy path."""
+    result = choose_metric(1,1)
+    assert result == METRIC_MTTR
+
+
+def test_chose_metric_loc() -> None:
+    """Test choose_metric happy path."""
+    result = choose_metric(1,2)
+    assert result != METRIC_MTTR
+
+
+
+
+
